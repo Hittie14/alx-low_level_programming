57ae1bd4e1d57332c4fa411ed1ef5179e998c4a1
Hittie14 <hitekaniem@gmail.com>
2023-06-20 06:31:24 -0700

    _putchar

--- _putchar.c
+++ _putchar.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 /**
- * _putchar - writes the charactor c to 
+ * _putchar - writes the charactor c to srdout
+ * @c: The charactor to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar (char c)
+(
+ 	return (write(1, &c, 1));
+)
